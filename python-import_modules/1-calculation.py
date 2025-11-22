5cebbd3bee3476815664e557466dbe65b663cb8b
ruslanhamidov <rhamidov202@gmail.com>
2025-11-22 20:47:26 +0000

    task 1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,7 +3,6 @@ from calculator_1 import add, sub, mul, div
 
 a = 10
 b = 5
-print()
 if __name__ == "__main__":
        print("{} + {} = {}".format(a, b, add(a, b)))
        print("{} - {} = {}".format(a, b, sub(a, b)))
