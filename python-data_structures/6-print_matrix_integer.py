2255cd48a653a31a99ccc2a8b0a113189deea513
ruslanhamidov <rhamidov202@gmail.com>
2025-12-09 17:04:21 +0400

    fix 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
-            print("{:d}".format(j), end=" " if col != row[-1] else "")
+            print("{:d}".format(j), end=" " if j != i[-1] else "")
         print()
