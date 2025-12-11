5b4b2f5a2c0cb5467e304da0891277b42e054c9f
ruslanhamidov <rhamidov202@gmail.com>
2025-12-11 21:00:23 +0000

    task 2

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        for i in range(x):
+            print("{:d}".format(my_list[i]), end="")
+    except Exception as e:
+        print(e)
