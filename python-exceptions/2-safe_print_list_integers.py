cb25ea445cfff00739e6a78816a0f7a3ed32fb22
ruslanhamidov <rhamidov202@gmail.com>
2025-12-11 21:03:56 +0000

    task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
         num = 0
         for i in range(x):
             print("{:d}".format(my_list[i]), end="")
-            num +=
+            num += 1
         return num
     except TypeError:
         raise TypeError
