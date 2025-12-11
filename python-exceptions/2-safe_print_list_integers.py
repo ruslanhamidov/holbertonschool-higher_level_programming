5470f6c929211cc8b97f9486d89f6efa6b8829c5
ruslanhamidov <rhamidov202@gmail.com>
2025-12-11 21:03:20 +0000

    task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     try:
+        num = 0
         for i in range(x):
             print("{:d}".format(my_list[i]), end="")
-    except Exception as e:
-        print(e.message)
+            num +=
+        return num
+    except TypeError:
+        raise TypeError
+    except IndexError:
+        raise IndexError
