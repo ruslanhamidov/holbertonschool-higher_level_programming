4f67336a45febd839a72dcc272d021da3ad5989a
ruslanhamidov <rhamidov202@gmail.com>
2025-12-11 21:37:25 +0000

    task 4

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    divs = []
+    for i in range(list_length):
+        try:
+            divs.append(my_list_1[i] / my_list_2[i])
+        except TypeError:
+            print("wrong type")
+            divs.append(0)
+            continue
+        except ZeroDivisionError:
+            print("division by 0")
+            divs.append(0)
+            continue
+        except IndexError:
+            print("out of range")
+            divs.append(0)
+            break
+    return divs
