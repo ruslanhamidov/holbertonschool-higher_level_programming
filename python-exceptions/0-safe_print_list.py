f5e8eba197099951692cd49eecd55f12a7d36a39
ruslanhamidov <rhamidov202@gmail.com>
2025-12-11 20:45:28 +0000

    task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,6 +4,6 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print(my_list[i], end="")
         print()
-        return i
+        return i + 1
     except KeyError:
         print("Index out of range")
