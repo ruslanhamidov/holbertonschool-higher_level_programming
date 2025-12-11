532bb77798d84e6034f10bcd4a9c71cbbbab9db1
ruslanhamidov <rhamidov202@gmail.com>
2025-12-11 21:39:53 +0000

    task 4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -15,7 +15,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
             divs.append(0)
-            break
+            continue
         finally:
             pass
     return divs
