7cf9316e14aa25dc7e2404bd3befa85c23ef6adc
root <ruslanhamidov>
2025-12-12 20:04:51 +0000

    task 0

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+'''
+Module which contains function to add two numbers
+'''
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    '''
+    Add two integers safe with exception
+    '''
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    a = int(a)
+    b = int(b)
+    return a + b
