373a545c1bf1c2896c33490bcd4aa768e1167dd8
ruslanhamidov <rhamidov202@gmail.com>
2025-12-24 16:46:43 +0400

    fix 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -31,7 +31,13 @@ class Square:
         """
         size setter
         """
-        self.__size = value
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
 
     def area(self):
         """
