6ad27f328e3c36c9ddd87da7a467c39754a0aefb
ruslanhamidov <rhamidov202@gmail.com>
2025-12-24 16:56:20 +0400

    task 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""
+Module with Square class
+"""
+
+
+class Square:
+    """
+    Square class that contains private size attribute
+    """
+    def __init__(self, size=0):
+        """
+        initilaize method
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        """
+        size getter
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        size setter
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif (value < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        returns current square area
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """
+        prints square with # character
+        """
+        for i in range(self.size):
+            for j in range(self.size):
+                print('#', end='')
+            print()
