6dbc6a3f6706d1c4ff912bd3d6e3593c60cb0a65
ruslanhamidov <rhamidov202@gmail.com>
2025-12-24 17:22:49 +0400

    task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -16,6 +16,8 @@ class Square:
             raise TypeError("size must be an integer")
         elif (size < 0):
             raise ValueError("size must be >= 0")
+        elif not isinstance(value, tuple) or tuple[0] < 0 or tuple[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__size = size
             self.__position = position
