907e5c68080076951aa221a19c7e75a2ddd9699d
ruslanhamidov <rhamidov202@gmail.com>
2025-12-24 15:56:48 +0400

    task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module with Square class
+"""
+
+
+class Square:
+    """
+    Square class that contains private size attribute
+    """
+    def __init__(self, size=0):
+        """
+        initilaize method
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
