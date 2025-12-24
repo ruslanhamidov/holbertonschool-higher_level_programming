e318bb1d9ce22acb352def29eedd3be32c279b87
ruslanhamidov <rhamidov202@gmail.com>
2025-12-24 15:50:00 +0400

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module with Square class
+"""
+
+
+class Square:
+    """
+    Square class that contains private size attribute
+    """
+    def __init__(self, size):
+        """
+        initilaize method
+        """
+        self.__size = size
+
