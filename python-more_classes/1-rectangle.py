0649e96cc5b8937eca1ab94612b5e2409330b27a
ruslanhamidov <rhamidov202@gmail.com>
2025-12-25 14:02:20 +0400

    task 1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+Module with Rectangle Class
+"""
+
+
+class Rectangle:
+    """
+    Rectangle class
+    """
+    def __init__(self, width=0, height=0):
+        """
+        Init method with width height
+        """
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """
+        width getter
+        """
+        return self.__width
+
+    @property
+    def height(self):
+        """
+        height getter
+        """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """
+        width setter
+        """
+        if not isinstance(value, int):
+            raise TypeError("weight must be an integer")
+        if value < 0:
+            raise ValueError("weight must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """
+        width setter
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
